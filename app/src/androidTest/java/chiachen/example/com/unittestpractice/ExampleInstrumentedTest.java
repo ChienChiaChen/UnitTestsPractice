d91a940710223842bf2114dd47e5641d205f2800
Jason_Chien <Jason_Chien@PerfectCorp.com>
2017-06-15 13:10:08 +0800

    First commit

---
+++ app/src/androidTest/java/chiachen/example/com/unittestpractice/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package chiachen.example.com.unittestpractice;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith (AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+	@Test
+	public void useAppContext() throws Exception {
+		// Context of the app under test.
+		Context appContext = InstrumentationRegistry.getTargetContext();
+
+		assertEquals("chiachen.example.com.unittestpractice", appContext.getPackageName());
+	}
+}
